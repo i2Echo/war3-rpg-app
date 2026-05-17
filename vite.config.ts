ad5e6602ddf230297b7e8b1b12ff55d4c599e3b7
kujohnln <sama2echo@gmail.com>
2026-05-17 10:56:43 +0800

    feat: 性能优化

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,23 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    target: 'es2020',
+    cssMinify: true,
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('src/data/formulas')) {
+            return 'engine'
+          }
+        },
+      },
+    },
+    minify: 'esbuild',
+    sourcemap: false,
+  },
+  css: {
+    devSourcemap: false,
+  },
 })
